7cfc177471a81f5d7b5b2ff2967658a6ac36add8
Stanislav-Kazankov <s.v.kazankov@mail.ru>
2021-02-26 12:22:22 +0900

    Делает интерфейс более доступным

--- components/control-panel.vue
+++ components/control-panel.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="control-panel">
+  <article class="control-panel">
+    <h3 class="control-panel__title">Панель добавления задачи</h3>
     <input class="control-panel__field"
+      id="field"
       type="text"
       name="todo"
       placeholder="Напишите задачу.."
       v-model:value="addedTask.title"
     >
+    <label class="control-panel__caption"
+      for="field"
+    >
+      Поле для ввода новой задачи
+    </label>
     <button type="button"
       @click="onBtnClick"
     >
     Добавить задачу
     </button>
-  </div>
+  </article>
 </template>
 
 <script>
@@ -36,6 +43,11 @@
   .control-panel {
     display: flex;
     flex-wrap: wrap;
+
+    &__title,
+    &__caption {
+      @include visually-hidden();
+    }
 
     &__field {
       margin-right: 10px;
